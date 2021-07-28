13a9079c5e6c44da935a6657102aa525ca00816e
DanielJDufour <daniel.j.dufour@gmail.com>
2021-07-28 17:21:11 -0400

    added parsing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { forEach, map } = require("advarr");
 /*
   examples:
   [row][column] is
@@ -28,7 +29,6 @@ function parseDimensions (str) {
   const re = /[A-Za-z]+/g;
   let arr;
   while ((arr = re.exec(str)) !== null) {
-    console.log({arr});
     const [match] = arr;
     dims[match] = {
       name: match
@@ -39,7 +39,63 @@ function parseDimensions (str) {
 
 const parseVectors = str => str.match(/\[[^\]]+\]/g);
 
+// "[row]" to "row"
+const removeBraces = str => str.startsWith("[") && str.endsWith("]") ? str.substring(1, str.length - 1) : str;
+
+// "(row)" to "row"
+const removeParentheses = str => str.startsWith("(") && str.endsWith(")") ? str.substring(1, str.length - 1) : str;
+
+// sort of like parsing a CSV except instead of " for quotes use (
+const matchSequences = str => str.match(/(\(.*?\)|[^\(,\s]+)(?=\s*,|\s*$)/g);
+
+const parseSequences = str => {
+  // unwrap [...]
+  str = removeBraces(str);
+
+  // unwrap (...)
+  str = removeParentheses(str);
+
+  const seqs = matchSequences(str);
+
+  if (seqs.length === 1) {
+    return {
+      type: "Vector",
+      dim: seqs[0]
+    };
+  } else {
+    return {
+      type: "Matrix",
+      parts: seqs.map(parseSequences)
+    }
+  }
+};
+
+function parse (str) {
+  const dims = parseDimensions(str);
+  const vectors = parseVectors(str);
+
+  vectors.map((vector, i) => {
+    // remove outer braces [ ... ]
+    const braceless = removeBraces(vector);
+
+    // remove parentheses if its exists
+    // as it's not required
+    const parenfree = removeParentheses(braceless);
+
+    console.log("parenfree:", [parenfree]);
+
+    const sequences = parseSequences(parenfree);
+    
+  });
+
+}
+
 module.exports = {
+  matchSequences,
+  parse,
   parseDimensions,
-  parseVectors
+  parseSequences,
+  parseVectors,
+  removeBraces,
+  removeParentheses,
 };
--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 const test = require("flug");
-const { parseDimensions, parseVectors } = require('./index');
+const {
+  matchSequences,
+  parse,
+  parseDimensions,
+  parseSequences,
+  parseVectors,
+  removeBraces,
+  removeParentheses
+} = require('./index');
 
 test("parseDimensions", ({ eq }) => {
   eq(
@@ -25,16 +33,64 @@ test("parseVectors", ({ eq }) => {
     parseVectors("[row][column]"),
     [ '[row]', '[column]' ]
   );
-})
+});
 
-// test("table", ({ eq }) => {
-//   const keypad = [
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9],
-//     ["*", 0, "#"]
-//   ];
-//   const syntax = "[row][column]";
-//   const layout = parse(syntax);
-//   eq(layout, )
-// });
+test("removing braces", ({ eq }) => {
+  eq(removeBraces("[row]"), "row");
+  eq(removeBraces("[(row)]"), "(row)");
+  eq(removeBraces("row"), "row");
+});
+
+test("removing parentheses", ({ eq }) => {
+  eq(removeParentheses("[row]"), "[row]");
+  eq(removeParentheses("(row)"), "row");
+  eq(removeParentheses("row"), "row");
+});
+
+test("matching sequences", ({ eq }) => {
+  eq(matchSequences("band,row,column"), ["band", "row", "column"]);
+  eq(matchSequences("band,(row,column)"), ["band", "(row,column)"]);
+});
+
+test("parsing one-level sequence", ({ eq }) => {
+  const str = "band,row,column";
+  const result = parseSequences(str);
+  eq(result, {
+    type: "Matrix",
+    parts: [
+      { type: "Vector", dim: "band" },
+      { type: "Vector", dim: "row" },
+      { type: "Vector", dim: "column" }
+    ]
+  });
+});
+
+test("parsing multi-level vectors", ({ eq }) => {
+  const str = "band,(row,column)";
+  const result = parseSequences(str);
+  eq(result, {
+    type: "Matrix",
+    parts: [
+      {type: "Vector", dim: "band" },
+      {
+        type: "Matrix",
+        parts: [
+          { type: "Vector", dim: "row" },
+          { type: "Vector", dim: "column" }
+        ]
+      }
+    ]
+  })
+});
+
+test("table", ({ eq }) => {
+  const keypad = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    ["*", 0, "#"]
+  ];
+  const syntax = "[row][column]";
+  const layout = parse(syntax);
+  console.log("layout:", layout);
+});
